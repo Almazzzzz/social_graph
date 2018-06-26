6c7fc4a2697ae6ec9f60493c0048fb41fc1646a6
MAI <matveev.rnd@gmail.com>
2018-06-26 16:54:21 +0300

    work vk login

--- page/app/routes.py
+++ page/app/routes.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session, escape
 from app import app
+import vk
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/login', methods=['GET', 'POST'])
@@ -7,14 +8,19 @@ def login():
     error = None
     if request.method == 'POST':
         if request.form['username'] and request.form['password']:
-            session = vk.AuthSession('6615751', request.form['username'], request.form['password'], scope='users, friends')
-            vk_api = vk.API(session, v='5.80')
-            user = vk_api.users.get(user_ids='0')
-            friends = vk_api.friends.get(user_id='0')
-            print(friends)
-            print(user)
+            try:
+                session = vk.AuthSession('6615751', request.form['username'], request.form['password'], scope='users, friends')
+            except vk.exceptions.VkAuthError:
+                error = 'Authorization error'
+            except:
+                error = 'Some error. Try again'
+            else:
+                vk_api = vk.API(session, v='5.80')
+                user = vk_api.account.getProfileInfo()
+                error = user #временно для проверки что хранится в user.
+ #               return redirect(url_for('hello'))
         else:
-            return redirect(url_for('hello'))
+            error = 'Empty Credentials. Please try again.'
     return render_template('login.html', error=error)
 
 @app.route('/hello')
