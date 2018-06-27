856f849609b771efb96ffbf293a53a08f514c3de
Almaz Badrtdinov <badrtdinov_a@mail.ru>
2018-06-27 16:59:28 +0300

    refactor: remane db users to people

--- seeds.py
+++ seeds.py
@@ -1,12 +1,12 @@
 from arango_db import connect_to_db, find_or_create_collection
 
 db = connect_to_db('social_graph_db')
-users = find_or_create_collection(db, 'users')
+people = find_or_create_collection(db, 'people')
 
 for i in range(10):
-    if not users.get({'_key': str(i)}):
-        data = {
+    if not people.has({'_key': str(i)}):
+        person = {
             '_key': str(i), 'first_name': f'Name {i}',
             'last_name': f'Surname {i}'
         }
-        users.insert(data)
+        people.insert(person)
--- users_report.py
+++
@@ -1,7 +0,0 @@
-from arango_db import connect_to_db, find_or_create_collection
-
-db = connect_to_db('social_graph_db')
-users = find_or_create_collection(db, 'users')
-
-for user in users:
-    print(f"User {user['_key']}: {user['first_name']} {user['last_name']}")
