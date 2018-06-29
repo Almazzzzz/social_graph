3ccf11226059e2101a89e63c3ff18a59a6a2498d
Almaz Badrtdinov <badrtdinov_a@mail.ru>
2018-06-29 16:39:24 +0300

    feat: add bfs function (working but not sure the right way)

--- vk_api.py
+++ vk_api.py
@@ -6,10 +6,11 @@ api = vk.API(session, v='5.78')
 
 
 def get_friends(user_id):
-    user = api.users.get(user_ids=user_id)
-    friends_list = api.friends.get(user_id=user_id)
+    # user = api.users.get(user_ids=user_id)
+    try:
+        friends = api.friends.get(user_id=user_id)
+    except vk.exceptions.VkAPIError:
+        friends = { 'items': [] }
+    friends_list = friends.get('items', [])[:10]
 
-    print(user, friends_list)
-
-
-get_friends(1001)
+    return friends_list
