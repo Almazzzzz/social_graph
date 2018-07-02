fcdecea940f3f068973b73d9169922f4f087573b
Almaz Badrtdinov <badrtdinov_a@mail.ru>
2018-07-02 21:09:00 +0300

    refactor: delete unneeded files

--- people_report.py
+++
@@ -1,8 +0,0 @@
-from arango_db import connect_to_db, find_or_create_collection
-
-db = connect_to_db('social_graph_db')
-people = find_or_create_collection(db, 'people')
-
-for person in people:
-    print(f"Person {person['_key']}: {person['first_name']} "
-          f"{person['last_name']}")
