483d50b1e82fafc517c64cd96e0e778b2df40107
Almaz Badrtdinov <badrtdinov_a@mail.ru>
2018-07-02 21:10:09 +0300

    feat: move all code to classes

--- arango_db.py
+++ arango_db.py
@@ -2,69 +2,63 @@ from arango import ArangoClient
 import settings
 
 
-def connect_to_db(db_name):
-    client = ArangoClient()
-    sys_db = client.db('_system', username='root',
-                       password=settings.arangodb_root_password)
+class ArangoDb:
+    def __init__(self, db_name):
+        self.client = ArangoClient()
+        self.sys_db = self.client.db('_system', username='root',
+                                     password=settings.arangodb_root_password)
+        self._private_create_database(db_name)
+        self.db = self.client.db(db_name, username=settings.arangodb_user,
+                                 password=settings.arangodb_user_password)
 
-    create_database(sys_db, db_name)
+    def _private_create_database(self, db_name):
+        if not self.sys_db.has_database(db_name):
+            self.sys_db.create_database(
+                name=db_name,
+                users=[
+                    {'username': settings.arangodb_user,
+                     'password': settings.arangodb_user_password,
+                     'active': True}
+                ]
+            )
 
-    db = client.db(db_name, username=settings.arangodb_user,
-                   password=settings.arangodb_user_password)
+    def find_or_create_collection(self, collection_name):
+        if self.db.has_collection(collection_name):
+            return self.db.collection(collection_name)
+        else:
+            return self.db.create_collection(collection_name)
 
-    return db
+    def find_or_create_graph(self, graph_name):
+        if self.db.has_graph(graph_name):
+            return self.db.graph(graph_name)
+        else:
+            return self.db.create_graph(graph_name)
 
+    @staticmethod
+    def find_or_create_vertex_collection(graph, collection_name):
+        if graph.has_vertex_collection(collection_name):
+            return graph.vertex_collection(collection_name)
+        else:
+            return graph.create_vertex_collection(collection_name)
 
-def create_database(sys_db, db_name):
-    if not sys_db.has_database(db_name):
-        sys_db.create_database(
-            name=db_name,
-            users=[
-                {'username': settings.arangodb_user,
-                 'password': settings.arangodb_user_password,
-                 'active': True}
-            ]
+    @staticmethod
+    def find_or_create_edge_definition(graph, definition_name,
+                                       vertex_collection_name):
+        if graph.has_edge_definition(definition_name):
+            return graph.edge_collection(definition_name)
+        else:
+            return graph.create_edge_definition(
+                edge_collection=definition_name,
+                from_vertex_collections=[vertex_collection_name],
+                to_vertex_collections=[vertex_collection_name]
+            )
+
+    @staticmethod
+    def traverse(graph, start_vertex):
+        graph.traverse(
+            start_vertex=start_vertex,
+            direction='outbound',
+            strategy='bfs',
+            edge_uniqueness='global',
+            vertex_uniqueness='global',
         )
-
-
-def find_or_create_collection(db, collection_name):
-    if db.has_collection(collection_name):
-        return db.collection(collection_name)
-    else:
-        return db.create_collection(collection_name)
-
-
-def find_or_create_graph(db, graph_name):
-    if db.has_graph(graph_name):
-        return db.graph(graph_name)
-    else:
-        return db.create_graph(graph_name)
-
-
-def find_or_create_vertex_collection(graph, collection_name):
-    if graph.has_vertex_collection(collection_name):
-        return graph.vertex_collection(collection_name)
-    else:
-        return graph.create_vertex_collection(collection_name)
-
-
-def find_or_create_edge_definition(graph, definition_name,
-                                   vertex_collection_name):
-    if graph.has_edge_definition(definition_name):
-        return graph.edge_collection(definition_name)
-    else:
-        return graph.create_edge_definition(
-            edge_collection=definition_name,
-            from_vertex_collections=[vertex_collection_name],
-            to_vertex_collections=[vertex_collection_name]
-        )
-
-
-def traverse(graph, start_vertex):
-    graph.traverse(
-        start_vertex=start_vertex,
-        direction='outbound',
-        strategy='bfs',
-        edge_uniqueness='global',
-        vertex_uniqueness='global',
-    )
--- main.py
+++ main.py
@@ -1,62 +1,76 @@
 from arango_db import *
-from vk_api import get_friends
-
-db = connect_to_db('social_graph_db')
-graph = find_or_create_graph(db, 'vk')
-users = find_or_create_vertex_collection(graph, 'users')
-friends = find_or_create_edge_definition(graph, 'friends', 'users')
+from vk_api import *
 
 
-def bfs(start_vk_id, goal_vk_id, stop_level=6):
-    level = 0
-    stop_condition = False
-    queue = [[None, start_vk_id, level]]
-    insert_user(start_vk_id)
+class SocialGraph:
 
-    while queue:
-        current = queue.pop(0)
-        print(f'Текущий: {current}')
-        id = current[1]
-        parent = current[0]
-        level = current[2] + 1
-        if level > stop_level:
-            break
-        insert_user(id)
+    def __init__(self, start_vk_id, goal_vk_id, stop_level=6):
+        self.start_vk_id = start_vk_id
+        self.goal_vk_id = goal_vk_id
+        self.stop_level = stop_level
+        self.connection = ArangoDb('social_graph_db')
+        self.graph = self.connection.find_or_create_graph('vk')
+        self.users = ArangoDb.find_or_create_vertex_collection(
+            self.graph, 'users'
+        )
+        self.friends = ArangoDb.find_or_create_edge_definition(
+            self.graph, 'friends', 'users'
+        )
 
-        if parent:
-            insert_friend(parent, id)
-        if id == goal_vk_id:
-            return True
+    def bfs(self):
+        vk_api = VkApi()
+        level = 0
+        stop_condition = False
+        queue = [[None, self.start_vk_id, level]]
+        self._private_insert_user(self.start_vk_id)
 
-        if not stop_condition:
-            friends_list = get_friends(id)
-            stop_condition = goal_vk_id in friends_list
-            if stop_condition:
-                queue = [[id, goal_vk_id, level]]
-            else:
-                ids_in_queue = [elem[1] for elem in queue]
-                queue.extend([[id, friend, level] for friend in friends_list if friend not in ids_in_queue])
+        while queue:
+            current = queue.pop(0)
+            print(f'Текущий: {current}')
+            id = current[1]
+            parent = current[0]
+            level = current[2] + 1
+            if level > self.stop_level:
+                break
+            self._private_insert_user(id)
 
-        print(f'Очередь: {len(queue)}')
-        print('')
+            if parent:
+                self._private_insert_friend(parent, id)
+            if id == self.goal_vk_id:
+                return True
 
-    return False
+            if not stop_condition:
+                friends_list = vk_api.get_friends(id)
+                stop_condition = self.goal_vk_id in friends_list
+                if stop_condition:
+                    queue = [[id, self.goal_vk_id, level]]
+                else:
+                    ids_in_queue = [elem[1] for elem in queue]
+                    queue.extend(
+                        [[id, friend, level] for friend in friends_list
+                            if friend not in ids_in_queue]
+                    )
+
+            print(f'Очередь: {len(queue)}')
+            print('')
+
+        return False
+
+    def _private_insert_user(self, vk_id):
+        if not self.users.has(str(vk_id)):
+            self.users.insert({'_key': str(vk_id)})
+
+    def _private_insert_friend(self, first_vk_id, last_vk_id):
+        if not (self.friends.has(f'{first_vk_id}-{last_vk_id}') or
+                self.friends.has(f'{last_vk_id}-{first_vk_id}')):
+            self.friends.insert({
+                '_key': f'{first_vk_id}-{last_vk_id}',
+                '_from': f'users/{first_vk_id}',
+                '_to': f'users/{last_vk_id}'
+            })
 
 
-def insert_user(vk_id):
-    if not users.has(str(vk_id)):
-        users.insert({'_key': str(vk_id)})
-
-
-def insert_friend(first_vk_id, last_vk_id):
-    if not (friends.has(f'{first_vk_id}-{last_vk_id}') or
-            friends.has(f'{last_vk_id}-{first_vk_id}')):
-        friends.insert({
-            '_key': f'{first_vk_id}-{last_vk_id}',
-            '_from': f'users/{first_vk_id}',
-            '_to': f'users/{last_vk_id}'
-        })
-
-
-result = bfs(1861235, 11821, 5)
-print(result)
+if __name__ == '__main__':
+    social_graph = SocialGraph(1861235, 11821, 5)
+    result = social_graph.bfs()
+    print(result)
