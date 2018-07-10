96057bbfc02e95033728cb8e1883ffd05d8cf068
Anatoliy Matveev <matveev.rnd+git@gmail.com>
2018-07-10 22:29:16 +0300

    Add work w/ cache

--- page/tmp.py
+++ page/tmp.py
@@ -3,8 +3,10 @@ import time
 def loop():
     while True:
         message = uwsgi.mule_get_msg()
-        print(message)
+        print(message.decode("utf-8")) #decode <class 'bytes'> to string
+        uwsgi.cache_set(message, 'inprogress')
         time.sleep(10)
+        uwsgi.cache_update(message, 'found')
 
 if __name__ == '__main__':
     loop()
