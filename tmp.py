a0cad24a09106fa21f80762f12777d56758c3841
MAI <matveev.rnd@gmail.com>
2018-07-11 12:02:57 +0300

    delete folder 'page', move files

--- tmp.py
+++ tmp.py
@@ -3,7 +3,7 @@ import time
 def loop():
     while True:
         message = uwsgi.mule_get_msg()
-        print(message.decode("utf-8")) #decode <class 'bytes'> to string
+        print(message.decode("utf-8").split('_')) #decode <class 'bytes'> to string and split to ['userID', 'searchUserID']
         uwsgi.cache_set(message, 'inprogress')
         time.sleep(10)
         uwsgi.cache_update(message, 'found')
